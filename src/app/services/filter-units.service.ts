a5ed32d010f3b1bf3e48b6728b3f4f37603e70fd
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-10 12:36:19 -0300

    feat: Add filter units service

---
+++ src/app/services/filter-units.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { UnitLocation } from '../types/unit-location.interface';
+
+const OPENING_HOURS = {
+  morning: {
+    first: '06',
+    last: '12'
+  },
+  afternoon: {
+    first: '12',
+    last: '18'
+  },
+  night: {
+    first: '18',
+    last: '23'
+  }
+}
+
+type HOUR_INDEXES = 'morning' | 'afternoon' | 'night';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterUnitsService {
+
+  constructor() { }
+
+  transformWeekday(day: number) {
+    switch (day) {
+      case 0:
+        return 'Dom.'
+      case 6:
+        return 'Sáb.'
+      default:
+        return 'Seg. à Sex.'
+    }
+  }
+
+  filterUnits(unit: UnitLocation, open_hour: string, close_hour: string) {
+
+    if (!unit.schedules) return true;
+
+    let open_hour_filter = parseInt(open_hour, 10);
+    let close_hour_filter = parseInt(close_hour, 10);
+
+    let today_weekday = this.transformWeekday(new Date().getDay());
+
+    for (let i = 0; i < unit.schedules.length; i++) {
+      let schedule_hour = unit.schedules[i].hour;
+      let schedule_weekday = unit.schedules[i].weekdays;
+
+      if (schedule_weekday === today_weekday) {
+        if (schedule_hour !== 'Fechada') {
+          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ');
+          let unit_open_hour_int = parseInt(unit_open_hour.replace('h', ''), 10);
+          let unit_close_hour_int = parseInt(unit_close_hour.replace('h', ''), 10);
+
+          if (unit_open_hour_int <= open_hour_filter && unit_close_hour_int >= close_hour_filter) return true;
+          else return false;
+        }
+      }
+    }
+    return false;
+  }
+
+  //Filtra as unidades por unidades abertas ou fechadas e também pelo período do dia:
+  filter(results: UnitLocation[], showClosed: boolean, hour: string): UnitLocation[] {
+    let intermediateResults = results;
+
+    if(!showClosed) {
+      intermediateResults = results.filter(location => location.opened === true);
+    }
+
+    if(hour) {
+      const OPEN_HOUR = OPENING_HOURS[hour as HOUR_INDEXES].first;
+      const CLOSE_HOUR = OPENING_HOURS[hour as HOUR_INDEXES].last;
+      return intermediateResults.filter(location => this.filterUnits(location, OPEN_HOUR, CLOSE_HOUR));
+    } else {
+      return intermediateResults;
+    }
+  }
+}
